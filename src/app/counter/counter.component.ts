0376f513505899515bd0ad07b840d2d2c28fad56
Martin Askestad <MartinAskestad@users.noreply.github.com>
2019-10-12 09:06:15 +0200

    add counter gui

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'svn-counter',
+  templateUrl: './counter.component.html',
+  styles: []
+})
+export class CounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
